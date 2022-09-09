a6c8b4b9fff792239f9346c0af5e926ff6cae4e9
dfsdanimaulana <danimaulana9f@gmail.com>
2022-09-09 21:12:55 +0700

    create rest folder

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,8 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    DB_ATLAS_DEV: Joi.string().required().description('Mongo DB Atlas url for development'),
+    DB_ATLAS_PROD: Joi.string().required().description('Mongo DB Atlas url for production'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -22,7 +23,15 @@ const envVarsSchema = Joi.object()
     SMTP_PORT: Joi.number().description('port to connect to the email server'),
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app')
+    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    CLOUDINARY_URL: Joi.string().required().description('cloudinary url'),
+    CLOUDINARY_API_SECRET: Joi.string().required().description('cloudinary api secret'),
+    CLOUDINARY_API_KEY: Joi.number().required().description('cloudinary api url'),
+    CLOUDINARY_NAME: Joi.string().required().description('cloudinary name'),
+    CLOUDINARY_UPLOAD_POST: Joi.string().required().description('cloudinary upload preset / folder to upload posts user'),
+    CLOUDINARY_UPLOAD_PIC: Joi.string()
+      .required()
+      .description('cloudinary upload preset / folder to upload profile image user')
   })
   .unknown()
 
@@ -36,8 +45,7 @@ module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    // url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
-    url: envVars.MONGODB_URL,
+    url: envVars.NODE_ENV === 'development' ? envVars.DB_ATLAS_DEV : envVars.DB_ATLAS_PROD,
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
@@ -61,5 +69,13 @@ module.exports = {
       }
     },
     from: envVars.EMAIL_FROM
+  },
+  cloudinary: {
+    url: envVars.CLOUDINARY_URL,
+    api_secret: envVars.CLOUDINARY_API_SECRET,
+    api_key: envVars.CLOUDINARY_API_KEY,
+    name: envVars.CLOUDINARY_NAME,
+    upload_post: envVars.CLOUDINARY_UPLOAD_POST,
+    upload_pic: envVars.CLOUDINARY_UPLOAD_PIC
   }
 }
